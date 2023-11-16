9b3a69965b105faa47a980f9855e48c5655c46a9
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2023-11-15 21:04:52 -0500

    Setting up the environment with gitignore added at the start

---
+++ UITestCodeWithChris/UITestCodeWithChris/UITestCodeWithChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestCodeWithChrisApp.swift
+//  UITestCodeWithChris
+//
+//  Created by Matt Crowe on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct UITestCodeWithChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
