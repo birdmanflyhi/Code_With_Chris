1b3548a083d59295c2b634c1191572f39c56d680
Matt Crowe <birdman@Matts-MacBook-Pro.local>
2023-11-16 16:09:38 -0500

    Completed Code With Chris and moved on to other apple documentation

--- UITestCodeWithChris/UITestCodeWithChris/ContentView.swift
+++ UITestCodeWithChris/UITestCodeWithChris/ContentView.swift
@@ -8,17 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+  @State private var showAlert = false
+
+  var body: some View {
+    VStack {
+      Image(systemName: "bird")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+
+
+      Text("Hello Friend")
+        .padding( /*@START_MENU_TOKEN@*/.all, 10.0 /*@END_MENU_TOKEN@*/)
+        .foregroundColor(Color.green)
+
+      Button("Click for an Alert") {
+        showAlert = true
+
+      }
+      .buttonStyle(.borderedProminent)
+      .padding(20.0)
+      .frame(height: 12.0)
+      .alert(isPresented: $showAlert) {
+        Alert(
+          title: Text("Alert the team"),
+          message: Text("Confirm you meant to do this"),
+          primaryButton: .default(
+            Text("Yes"),
+            action: saveData
+          ),
+          secondaryButton: .destructive(
+            Text("No"),
+            action: deleteConfirm
+          )
+        )
+      }
+
+      //.badge("Test")
+
     }
+
+  }
+
+}
+func greeting() {
+  print("Hello Old Friend")
+
+}
+
+func saveData() {
+  print("Saving Data")
+}
+
+func deleteConfirm() {
+  print("Do Nothing")
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
